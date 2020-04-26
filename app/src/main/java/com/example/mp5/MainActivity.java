2e7db4d3afa0a853b06d17388dbb767e3ab69a7f
zhekaig2 <zhekaig2@illinois.edu>
2020-04-26 12:22:26 -0500

    equation 随机生成加减乘除两个数（100以内一位小数）需完善

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.mp5;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.text.DecimalFormat;
+import java.util.Random;
+
 public class MainActivity extends AppCompatActivity {
 
     /* the right result. */
-    private int rightResult;
+    private double rightResult;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,10 +17,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public boolean result(int userResult) {
+    public boolean result(double userResult) {
         if (userResult == rightResult) {
             return true;
         }
         return false;
+    }
+
+    private String getpractice() {
+        Random randomnumber = new Random();
+        DecimalFormat form = new DecimalFormat("0.0");
+        String number1 = form.format(randomnumber.nextDouble() * 100);
+        String number2 = form.format(randomnumber.nextDouble() * 100);
+        double numbera = Double.parseDouble(number1);
+        double numberb = Double.parseDouble(number2);
+        int number = randomnumber.nextInt(4);
+        switch (number) {
+            case 0:
+                rightResult = numbera + numberb;
+                return number1 + "+" + number2 + "=";
+            case 1:
+                rightResult = numbera - numberb;
+                return number1 + "-" + number2 + "=";
+            case 2:
+                rightResult = numbera * numberb;
+                return number1 + "*" + number2 + "=";
+            case 3:
+                /*需要完善除法小数点问题*/
+                rightResult = numbera / numberb;
+                return number1 + "/" + number2 + "=";
+        }
+        return "Wrong";
     }
 }
