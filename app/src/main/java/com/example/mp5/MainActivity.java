0f9656cdc87cc38aa80706308cf35c2ac0d98d47
caohanzhi <hanzhic3@illinois.edu>
2020-04-26 22:25:07 +0800

    boolean setting

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -5,9 +5,19 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    /* the right result. */
+    private int rightResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public boolean result(int userResult) {
+        if (userResult == rightResult) {
+            return true;
+        }
+        return false;
     }
 }
