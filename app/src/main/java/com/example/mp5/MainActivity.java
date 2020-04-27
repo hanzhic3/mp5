30868cd8f524837cd02c0c56ffe91261daad9605
caohanzhi <hanzhic3@illinois.edu>
2020-04-27 22:26:18 +0800

    setOnClickListener

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 
@@ -15,14 +17,13 @@ public class MainActivity extends AppCompatActivity {
     /* the right result. */
     private double rightResult;
 
+    /* the user result. */
+    private double userResult;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-
-    public boolean result(double userResult) {
-        return userResult == rightResult;
     }
 
     @SuppressLint("SetTextI18n")
@@ -49,5 +50,35 @@ public class MainActivity extends AppCompatActivity {
                 rightResult = Double.parseDouble(form.format(number1 / number2));
                 question.setText(numbera + "/" + numberb + "=");
         }
+    }
+
+    public void afterDone() {
+        if (userResult == rightResult) {
+            findViewById(R.id.right).setVisibility(View.VISIBLE);
+            findViewById(R.id.practice).setVisibility(View.VISIBLE);
+            findViewById(R.id.end).setVisibility(View.VISIBLE);
+        } else if (userResult != rightResult) {
+            findViewById(R.id.wrong).setVisibility(View.VISIBLE);
+            findViewById(R.id.practice).setVisibility(View.VISIBLE);
+            findViewById(R.id.end).setVisibility(View.VISIBLE);
+        }
+
+    }
+
+    public void getResult() {
+        Button done = findViewById(R.id.done);
+        done.setOnClickListener(v -> {
+            afterDone();
+        });
+
+        Button practice = findViewById(R.id.practice);
+        practice.setOnClickListener(v -> {
+            getpractice();
+        });
+
+        Button end = findViewById(R.id.end);
+        end.setOnClickListener(v -> {
+            finish();
+        });
     }
 }
