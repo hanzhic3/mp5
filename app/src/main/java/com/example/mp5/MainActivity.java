91b7b832e22b1fa8caebcddb96b3d32419f92f8f
zhekaig2 <zhekaig2@illinois.edu>
2020-05-01 11:27:33 -0500

    getpractice

--- app/src/main/java/com/example/mp5/MainActivity.java
+++ app/src/main/java/com/example/mp5/MainActivity.java
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        getpractice();
     }
 
     @SuppressLint("SetTextI18n")
